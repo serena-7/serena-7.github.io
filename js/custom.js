e75a78864618dfbb5e5f46ef67094b03d0fb73cf
serena-7 <serena.software.engineer@gmail.com>
2022-07-15 13:15:33 -0600

    portfolio and contact working

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,24 @@ $(function()
     new WOW().init();
     $(".rotate").textrotator();
 })
+
+$("#send").click(function() {
+	var name = $("#name").val();
+	var email = $("#email").val();
+	var message = $("#message").val();
+	if (name == '' || email == '' || message == '') {
+		alert('Please fill in all fields!')
+	} else {
+		print('Got Here')
+		$.post("contact_form.php", {
+			name1: name,
+			email1: email,
+			message1: message
+		}, function(data){
+			alert(data)
+			if (data == "Your request has been received, Serena will get back to you soon.") {
+				$("#contact-form")[0].reset()
+			}
+		})
+	}
+})
